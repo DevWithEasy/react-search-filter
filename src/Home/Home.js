bc67383e296cb9beb1200963bf63fb49b1fd512f
DevWithEasy <devwitheasy@gmail.com>
2022-09-10 09:40:09 +0600

    home page edit

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -6,6 +6,12 @@ const Home = () => {
         <div className="home">
             <div className="main">
                 <h1>Demo</h1>
+                <p className="">
+                    Project 01 Basic search fuction of sigle key.
+                </p>
+                <p className="">
+                    Project 02 Multi search fuction of multiple keys.
+                </p>
                 <div className="router">
                     <Link to='/project01' className='home_link'>Project 01</Link>
                     <Link to='/project02' className='home_link'>Project 02</Link>
